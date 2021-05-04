004148255452064ff06e6e91d75bc19744cc10a4
msfauzan <sabran.fauzan.14@gmail.com>
2021-05-04 11:49:41 +0700

    tugasakhir

---
+++ database/migrations/2021_05_04_022259_create_designs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDesignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('designs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_barang');
+            $table->string('nama_designer');
+            $table->integer('tarif');
+            $table->string('kontak_designer');
+            $table->string('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('designs');
+    }
+}
